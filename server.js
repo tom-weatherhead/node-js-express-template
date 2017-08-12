1ad446a12dceecee4ff6a761bfbed16e28c4e54f
Tom Weatherhead <tom_weatherhead@hotmail.com>
2017-08-12 16:49:25 -0600

    Added the dev tools

--- server.js
+++ server.js
@@ -1,64 +1,16 @@
+// node-js-express-template/server.js
+
 // An example of a simple Express.js Web server.
 // Tom Weatherhead - August 1, 2017
 
 // require('rootpath')();
-const express = require('express');
-const app = express();
+const app = require('./app');
 
-// **** Server Listen Port Configuration: Begin ****
+const config = require('./config');			// I.e. ./config.json
 
-// Set the server listen port: Method 1:
-// const serverListenPort = 3000;
+const serverListenPort = config.listenPort || 3000;
 
-// Set the server listen port: Method 2: Factor the data (the port number) out of the code:
-const config = require('./config.json');
-const serverListenPort = config.serverListenPort || 3000;
-
-// **** Server Listen Port Configuration: End ****
-
-// **** Cross-Origin Resource Sharing: Begin ****
-
-// See https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
-// See https://enable-cors.org/server_expressjs.html
-
-// General:
-
-// app.use(function(req, res, next) {
-	// res.header("Access-Control-Allow-Origin", "*");
-	// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	// next();
-// });
-
-// Minimal:
-
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "null");
-	res.header("GET");
-	next();
-});
-
-// **** Cross-Origin Resource Sharing: End ****
-
-// **** Request Event Handlers: Begin ****
-
-app.get('/', function (req, res) {
-	var result = {
-		message: 'GitHub!',
-		number: 343
-	};
-
-	console.log('GET / : Responding with JSON result:', result);
-	res.json(result);
-});
-
-app.get('/error', function (req, res) {
-	console.error('GET /error : Responding with HTTP status code 500 : Internal Server Error.');
-	res.status(500).send('Boom; HTTP status code code 500. Internal server error.');
-});
-
-// **** Request Event Handlers: End ****
-
-// **** Start the Server: Begin ****
+// Start the server:
 
 var server = app.listen(serverListenPort, function () {
 	let host = server.address().address;
@@ -69,7 +21,5 @@ var server = app.listen(serverListenPort, function () {
 
 	console.log('The Express.js server is listening at http://%s:%s (protocol %s)', host, server.address().port, server.address().family);
 });
-
-// **** Start the Server: End ****
 
 // End of File.
